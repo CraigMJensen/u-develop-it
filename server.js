8dc0a3ee2ca3705bcbc19ddf735ee9de3f0878e2
CraigJensen <cmjensen82@gmail.com>
2022-01-25 15:12:50 -0600

    Joined tables with foreign keys, tested with insomnia

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name
+               AS party_name
+               FROM candidates
+               LEFT JOIN parties
+               ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -40,7 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 // GET single candidate by ID
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`;
 
   const params = [req.params.id];
 
@@ -91,7 +100,7 @@ app.post('/api/candidate', ({ body }, res) => {
     return;
   }
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-    VALUES (?,?,?)`;
+              VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
